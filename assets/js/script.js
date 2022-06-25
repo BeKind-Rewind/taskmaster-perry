e46387b5a70e1d464dc2cf5cb26f69a7818f3f92
BeKind-Rewind <challenge641@gmail.com>
2022-06-25 14:39:48 -0500

    created cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,29 +46,6 @@ var saveTasks = function() {
 };
 
 $(".list-group").on("click", "p", function(){
-  $(".list-group").on("blur", "textarea", function(){
-    //get the textarea's current value/text
-    var text = $(this)
-      .val()
-      .trim();
-    // get the parent ul's id attribute
-    var status = $(this)
-    .closest(".list-group")
-    .attr("id")
-    .replace("list-", "");
-    // get the task's position in the list of other li elements
-    var index = $(this)
-      .closest(".list-group-item")
-      .index();
-    tasks[status][index].text = text;
-    saveTasks();
-    // recreate p element
-    var taskP = $("<p>")
-      .addClass("m-1")
-      .text(text);
-    // replace textarea with p element
-    $(this).replaceWith(taskP);
-  });
   var text = $(this)
     .text()
     .trim();
@@ -77,6 +54,29 @@ $(".list-group").on("click", "p", function(){
     .val(text);
     $(this).replaceWith(textInput);
     textInput.trigger("focus");
+});
+
+$(".list-group").on("blur", "textarea", function(){
+  //get the textarea's current value/text
+  var text = $(this).val();
+  // get the parent ul's id attribute
+  var status = $(this)
+  .closest(".list-group")
+  .attr("id")
+  .replace("list-", "");
+  // get the task's position in the list of other li elements
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+  // update task in array and re-save to localstorage
+  tasks[status][index].text = text;
+  saveTasks();
+  // recreate p element
+  var taskP = $("<p>")
+    .addClass("m-1")
+    .text(text);
+  // replace textarea with p element
+  $(this).replaceWith(taskP);
 });
 
 // due date clicked
@@ -94,32 +94,33 @@ $(".list-group").on("click", "span", function(){
   $(this).replaceWith(dateInput);
   // automatically focus on new element
   dateInput.trigger("focus");
-  $(".list-group").on("blur", "input[type='text']", function(){
-    // get current text
-    var date = $(this)
-      .val()
-      .trim();
-    //get the parent ul's id attribute
-    var status = $(this)
-      .closest(".list-group")
-      .attr("id")
-      .replace("list-", "");
-    // get the task's position in the list of other li elements
-    var index = $(this)
-      .closest(".list-group-item")
-      .index();
-    // update task in array and resave to local storage
-    tasks[status][index].date = date;
-    saveTasks();
-    // reactreate span element with bootstrap classes
-    var taskSpan = $("<span>")
-      .addClass("badge badge-primary badge-pill")
-      .text(date);
-    // replace input with span element
-    $(this).replaceWith(taskSpan);
-  });
 });
 
+// value of due date was changed
+$(".list-group").on("blur", "input[type='text']", function(){
+  // get current text
+  var date = $(this)
+    .val()
+    .trim();
+  //get the parent ul's id attribute
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+  // get the task's position in the list of other li elements
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+  // update task in array and resave to local storage
+  tasks[status][index].date = date;
+  saveTasks();
+  // reactreate span element with bootstrap classes
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+    // replace input with span element
+    $(this).replaceWith(taskSpan);
+});
 
 
 // modal was triggered
