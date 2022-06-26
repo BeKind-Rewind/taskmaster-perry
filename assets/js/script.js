2e0f9fb81bc5622c904d9626e5fc1a6328cf4ae3
BeKind-Rewind <challenge641@gmail.com>
2022-06-25 21:53:01 -0500

    added color for imminent due date and overdue date

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,8 +11,29 @@ var createTask = function(taskText, taskDate, taskList) {
     .text(taskText);
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
+  // check due date
+  auditTask(taskLi);
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
+};
+
+var auditTask = function(taskEl) {
+  // get date from task element
+  var date = $(taskEl).find("span")
+  .text()
+  .trim();
+  // convert a moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+  // remove old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new class if task is near/over due date
+  if (moment().isAfter(time)){
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
 };
 
 var loadTasks = function() {
@@ -218,6 +239,8 @@ $(".list-group").on("change", "input[type='text']", function() {
     .addClass("badge badge-primary badge-pill")
     .text(date);
     $(this).replaceWith(taskSpan);
+  // pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // remove all tasks
@@ -229,6 +252,8 @@ $("#remove-tasks").on("click", function() {
   console.log(tasks);
   saveTasks();
 });
+
+
 
 // load tasks for the first time
 loadTasks();
